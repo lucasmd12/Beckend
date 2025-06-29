692326b78cbe3799da5013120e852c9361d6258b
lucasmd12 <lucasjuzahyder@gmail.com>
2025-06-29 19:17:13 -0300

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -80,7 +80,7 @@ class FirebaseConfig {
         credential: admin.credential.cert({
           projectId: FIREBASE_PROJECT_ID,
           clientEmail: FIREBASE_CLIENT_EMAIL,
-          privateKey: FIREBASE_PRIVATE_KEY,
+          privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         }),
       });
 
